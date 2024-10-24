65d2b801d748b571f018b795a8f8452f69e1e927
oninvis <deoreom8@gmail.com>
2024-10-24 15:19:24 +0000

    added HTML and CSS

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask 
-app = Flask(__name__)
-@app.route("/")
-def hello():
-  return "Hello Om!"
+from flask import Flask , render_template 
 
-if __name__ == "__main__":
-  app.run('0.0.0.0',debug = True)
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0',debug=True)  
