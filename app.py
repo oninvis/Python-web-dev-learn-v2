18096a2f887dc3e8a903939b59e94ccb8ed3f01a
oninvis <deoreom8@gmail.com>
2024-10-25 09:00:38 +0000

    Used jsonify

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template 
+from flask import Flask , render_template , jsonify
 
 app = Flask(__name__)
 JOBS = [
@@ -31,6 +31,11 @@ JOBS = [
 @app.route('/')
 def index():
     return render_template('home.html',jobs =JOBS,company_name = "Jovian")
+
+
+@app.route("/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0',debug=True)  
